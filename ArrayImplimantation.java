03d98fe478e012f115562e15f07646b9430afc59
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-22 01:24:38 +0530

    10 minitues Only

---
+++ ArrayImplimantation.java
@@ -0,0 +1,14 @@
+/**
+ * ArrayImplimantation
+ */
+public class ArrayImplimantation {
+
+    public static void main(String[] args) {
+        int [] arr = {10,20,30,40,50};
+        char [] char1 = {'A','B','C'};
+
+        System.out.println(arr[2]);
+        System.out.println(arr[1]+arr[3]);
+        System.out.println(char1[2]);
+    }
+}
