8259a666b3afbae626f88faf9fcd48d0da0ec00d
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-23 01:16:20 +0530

    Update

--- ArrayInsertData.java
+++ ArrayInsertData.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class ArrayInsertData {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        int arr [] = new int[5];
+        int size=5;
+        System.out.println("Data input :");
+        for(int i = 0 ; i <size; i++){
+            arr[i] = input.nextInt();
+        }
+        for(int j = 0 ; j <size; j++){
+            System.out.println(arr[j]);
+        }
     }
 }
